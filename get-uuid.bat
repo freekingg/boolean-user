3d249e805260d00e4f3e6390ffac62df723fa519
记忆的时间差 <xianxin001@gmail.com>
2025-02-27 21:11:38 +0800

    Add files via upload

--- get-uuid.bat
+++ get-uuid.bat
@@ -1,8 +1,10 @@
 @echo off
+chcp 65001
+
 wmic csproduct get uuid | clip
-echo �����Ѹ��Ƶ������壬ֱ�ӽ����ݷ��͸�����Ա��
+echo 内容已复制到剪贴板，直接将内容发送给管理员。
 
 wmic csproduct get uuid > uuid.txt
-echo �����ѱ��浽uuid.txt�ļ������Բ鿴�ļ��ڵ����ݡ�
+echo 内容已保存到uuid.txt文件，可以查看文件内的内容。
 
 pause
